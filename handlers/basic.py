51bc9af595e972f12eaa450e80e0d4ef9a256144
Roman Kiper <kiper.r84@gmail.com>
2023-09-12 23:56:35 +0300

    second- commit

--- handlers/basic.py
+++ handlers/basic.py
@@ -7,8 +7,6 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 
 async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f"Привет {message.from_user.first_name}. Рад тебя видеть!")
-    await message.answer(f"Привет {message.from_user.first_name}. Рад тебя видеть!")
-    await message.reply(f"Привет {message.from_user.first_name}. Рад тебя видеть!")
 
 
 
