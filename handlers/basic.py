13bb0374ff2497589bdaf6b76a240e5a61c33262
Roman Kiper <kiper.r84@gmail.com>
2024-01-24 22:21:26 +0300

    24

--- handlers/basic.py
+++ handlers/basic.py
@@ -58,8 +58,9 @@ async def get_second_menu(callback: types.CallbackQuery):
 
 @router.message(Command(commands='description'))
 async def get_description(message: Message):
-    await message.answer(text=LEXICON_RU['/description'])
     await message.delete()
+    await message.answer(text=LEXICON_RU['/description'])
+
 
 
 @router.message()
