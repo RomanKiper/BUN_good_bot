ce3214b49fe4ac8156914de4ccf39c3d80786701
Roman Kiper <kiper.r84@gmail.com>
2024-01-29 23:13:48 +0300

    29

--- keyboards/inline/inline_faq.py
+++ keyboards/inline/inline_faq.py
@@ -1,23 +1,23 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-# from lexicon.lexicon import LEXICON_PRICE, LEXICON_btn_faq
-#
-# def get_markup_faq(width: int, *args, **kwargs) -> InlineKeyboardMarkup:
-#     kb_builder = InlineKeyboardBuilder()
-#     buttons: list[InlineKeyboardButton] = []
-#     # Заполняем список кнопками из аргументов args и kwargs
-#     if args:
-#         for button in args:
-#             buttons.append(InlineKeyboardButton(
-#                 text=LEXICON_btn_faq[button] if button in LEXICON_btn_faq else button,
-#                 callback_data=button
-#             ))
-#     if kwargs:
-#         for button, text in kwargs.items():
-#             buttons.append(InlineKeyboardButton(
-#                 text=text,
-#                 callback_data=button
-#             ))
-#     kb_builder.row(*buttons, width=width)
-#     return kb_builder.as_markup()
+from lexicon.lexicon import LEXICON_PRICE, LEXICON_btn_faq
+
+def get_markup_faq(width: int, *args, **kwargs) -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON_btn_faq[button] if button in LEXICON_btn_faq else button,
+                callback_data=button
+            ))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button
+            ))
+    kb_builder.row(*buttons, width=width)
+    return kb_builder.as_markup()
 
